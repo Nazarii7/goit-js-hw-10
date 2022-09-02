7b6f36e167fe4b52f9a9d7187f16111e0f3d40e8
Nazarii7 <99838823+Nazarii7@users.noreply.github.com>
2022-09-02 12:04:07 +0000

    Deploying to gh-pages from @ Nazarii7/goit-js-hw-10@0b0d0bc1dc535a79a4f8ee32159236fc3ae34adb 🚀

---
+++ index.10000399.js
@@ -0,0 +1,2 @@
+fetch("https://restcountries.com/v3.1/all").then((function(n){return n.json()})).then((function(n){console.log(n)})).catch((function(n){console.log(n)}));
+//# sourceMappingURL=index.10000399.js.map
